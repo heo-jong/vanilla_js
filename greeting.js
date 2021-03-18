6276b639fe0c845a51f1050858d447bde0d6ca4d
heo-jong <1004kss1004@gmail.com>
2021-03-18 22:18:22 +0900

    Upgrade_1

--- greeting.js
+++ greeting.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+  greeting = document.querySelector(".js-greetings"),
+  delGreeting = document.querySelector(".del-greetings");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
@@ -21,9 +22,32 @@ function askForName() {
   form.addEventListener("submit", handleSubmit);
 }
 
+function resetTodo() {
+  const li = document.querySelectorAll("li");
+  li.forEach(function (liDel) {
+    liDel.remove();
+  });
+}
+
+function resetAll(event) {
+  const currentUser = localStorage.getItem(USER_LS);
+  localStorage.removeItem(USER_LS, currentUser);
+  localStorage.removeItem(TODOS_LS);
+  greeting.classList.remove(SHOWING_CN);
+  delGreeting.classList.remove(SHOWING_CN);
+  input.value = null;
+  resetTodo();
+  loadName();
+  history.go(0);
+}
+
+const resetAllBtn = document.querySelector(".del-greetings");
+resetAllBtn.addEventListener("click", resetAll);
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
+  delGreeting.classList.add(SHOWING_CN);
   greeting.innerText = `Do Your Best, ${text}`;
 }
 
--- todo.js
+++ todo.js
@@ -13,6 +13,7 @@ function deleteToDo(event) {
   const cleanToDos = toDos.filter(function (toDo) {
     return toDo.id !== parseInt(li.id);
   });
+
   toDos = cleanToDos;
   saveToDos();
 }
